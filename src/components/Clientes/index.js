5ad9924e5f2d54a25c71089b867ba7cfe1344a80
Dandara Sousa <dandaralsousa@gmail.com>
2021-07-10 00:08:17 -0300

    corrigindo possivel erro do target

--- src/components/Clientes/index.js
+++ src/components/Clientes/index.js
@@ -9,7 +9,7 @@ const Clientes = () => {
                         
                         <div className="texto">
                             <h1>Farmácias Droga Félix</h1>
-                            <a href="https://www.instagram.com/farmaciasdrogafelix/" target="_blank"><button className="botao" >Ver Mais</button></a>
+                            <a href="https://www.instagram.com/farmaciasdrogafelix/" target="_blank" rel="noreferrer"><button className="botao" >Ver Mais</button></a>
                         </div>
                         <img className="banner" src="https://i.ibb.co/6DdN6Tg/dorga-felix.png" alt="dorga-felix"/>
                         </div>
@@ -20,7 +20,7 @@ const Clientes = () => {
                         <div>
                         <div className="texto">
                             <h1>Pele Atelier - Estética Avançada</h1>
-                            <a href="https://www.instagram.com/peleatelier/" target="_blank"><button className="botao" >Ver Mais</button></a>
+                            <a href="https://www.instagram.com/peleatelier/" target="_blank" rel="noreferrer"><button className="botao" >Ver Mais</button></a>
                         </div>
                         <img className="banner" src="https://i.ibb.co/8zSJHRP/pele-atelier.png" alt="imagem de limpeza de pele"/>
                         </div>
@@ -31,7 +31,7 @@ const Clientes = () => {
                         <div>
                         <div className="texto">
                             <h1>Dr. Paulo Régis - Cirurgia Plástica </h1>
-                            <a href="https://www.instagram.com/pauloregiscirurgiaplastica/?hl=pt-br" target="_blank"><button className="botao" >Ver Mais</button></a>
+                            <a href="https://www.instagram.com/pauloregiscirurgiaplastica/?hl=pt-br" target="_blank" rel="noreferrer"><button className="botao" >Ver Mais</button></a>
                         </div>
                         <img className="banner" src="https://i.ibb.co/BBbd1dG/paulo-regis.png" alt="imagem de três rostos femininos"/>
                         </div>
@@ -42,7 +42,7 @@ const Clientes = () => {
                         <div>
                         <div className="texto">
                             <h1>Policlínica Dr. Luis Pessoa</h1>
-                            <a href="https://www.instagram.com/policlinicadrluispessoa/" target="_blank"><button className="botao" >Ver Mais</button></a>
+                            <a href="https://www.instagram.com/policlinicadrluispessoa/" target="_blank" rel="noreferrer"><button className="botao" >Ver Mais</button></a>
                         </div>
                         <img className="banner" src="https://i.ibb.co/S6SSHRx/policlinica.png" alt="imagem de um médico"/>
                         </div>
@@ -53,7 +53,7 @@ const Clientes = () => {
                         <div>
                         <div className="texto">
                             <h1>Celu - Psicologia </h1>
-                            <a href="https://www.instagram.com/celupsicologia/" target="_blank"><button className="botao" >Ver Mais</button></a>
+                            <a href="https://www.instagram.com/celupsicologia/" target="_blank" rel="noreferrer"><button className="botao" >Ver Mais</button></a>
                         </div>
                         <img className="banner" src="https://i.ibb.co/G0W0K0w/celu.png" alt="imagem de pessoas de mãos dadas"/>
                         </div>
@@ -64,7 +64,7 @@ const Clientes = () => {
                         <div>
                         <div className="texto">
                             <h1>AF Fysical - Academia</h1>
-                            <a href="https://www.instagram.com/affysicalacademia/" target="_blank"><button className="botao" >Ver Mais</button></a>
+                            <a href="https://www.instagram.com/affysicalacademia/" target="_blank" rel="noreferrer"><button className="botao" >Ver Mais</button></a>
                         </div>
                         <img className="banner" src="https://i.ibb.co/bQbf1tg/academia.png" alt="imagem de academia"/>
                         </div>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,17 +10,17 @@ const Footer = () => {
     <div className="footer">
         <div className="social-media">
             
-        <a href="https://www.facebook.com/brasilisagencia/" target="_blank">     
+        <a href="https://www.facebook.com/brasilisagencia/" target="_blank" rel="noreferrer">     
             <img className="imgs" src={Img1} alt="Imagem do logo do Facebook" width="25px" height="25px"/>
 
         </a>
 
-        <a href="https://www.instagram.com/agenciabrasilis/" target="_blank">     
+        <a href="https://www.instagram.com/agenciabrasilis/" target="_blank" rel="noreferrer">     
             <img className="imgs"src={Img2} alt="Imagem do logo do Instagram" width="25px" height="25px"/>
 
         </a>
 
-        <a href="https://www.linkedin.com/in/agência-brasilis-comunicação-805565150/" target="_blank">       
+        <a href="https://www.linkedin.com/in/agência-brasilis-comunicação-805565150/" target="_blank" rel="noreferrer">       
             <img className="imgs"src={Img3} alt="Imagem do logo do Linkedin" width="25px" height="25px"/>
         
         </a>
@@ -28,7 +28,7 @@ const Footer = () => {
         </div>
 
         <div className="autora">
-        <p>Desenvolvido por <a href="https://www.linkedin.com/in/dandarasousa/" target="_blank"><strong>Dandara Sousa</strong></a>. Todos os direitos revervados.</p>
+        <p>Desenvolvido por <a href="https://www.linkedin.com/in/dandarasousa/" target="_blank" rel="noreferrer"><strong>Dandara Sousa</strong></a>. Todos os direitos revervados.</p>
         </div>
 
     </div>
